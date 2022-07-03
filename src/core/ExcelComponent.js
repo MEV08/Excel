2b75d88d0fa8da09f80c049af5918734e9d5d530
Eugene Mashkovtsev <eugenemashkovtsev@MacBook-Air-Eugene.local>
2022-07-04 02:17:22 +0300

    finished base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,15 +4,31 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
+    this.prepare();
   }
-  // Возвращает шиблон компонента
+  // Настраиваем компонент до init
+  prepare() {}
+  // Возвращает шаблон компонента
   toHTML() {
     return '';
   }
+  // уведомление слушателей про события event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+  // инициализируем компонент и добавлем dom слушатели
   init() {
     this.initDomListeners();
   }
+  // удалем компонент + чистка слушателей
   destroy() {
     this.removeDomListeners();
+    this.unsubscribers.forEach(unsub => unsub());
   }
 }
